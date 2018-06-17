233809a2fa047299f7c5af075f98fd03bb8eb6e9
Dan Drust <dan.drust@protrainings.com>
2018-06-16 22:53:26 -0400

    Implement return

--- lib/function_call/return.rb
+++ lib/function_call/return.rb
@@ -7,7 +7,64 @@ class FunctionCall
     end
 
     def to_assembly
-      
+      <<~CODE
+      @LCL
+      D=M
+      @R13 // Temporary variable FRAME
+      M=D
+
+      @5
+      D=A
+      @R13
+      A=A-D // A register points to FRAME-5
+      D=M // D register holds address at FRAME-5
+      @R14 // Temporary variable RET
+      M=D
+
+      // Set address at ARG to value at SP-1
+      @SP
+      A=A-1
+      D=M // return value is now in D
+      @ARG
+      A=M
+      M=D 
+
+      // SP = ARG+1
+      @ARG
+      D=M+1
+      @SP
+      M=D
+
+      @R13 // FRAME
+      D+M
+      @THAT
+      M=D-1
+
+      @R13 // FRAME
+      D+M
+      @2
+      D=D-A
+      @THIS
+      M=D
+
+      @R13 // FRAME
+      D+M
+      @3
+      D=D-A
+      @ARG
+      M=D
+
+      @R13 // FRAME
+      D+M
+      @4
+      D=D-A
+      @LCL
+      M=D
+
+      // Go to return address
+      @R14
+      0;JMP
+      CODE
     end
   end
 end

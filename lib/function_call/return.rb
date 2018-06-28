776bdbb09d3e08befa5e8c5b9511a7716af8adad
Dan Drust <dan.drust@protrainings.com>
2018-06-27 22:58:53 -0400

    fix various bugs in return call code

--- lib/function_call/return.rb
+++ lib/function_call/return.rb
@@ -16,14 +16,14 @@ class FunctionCall
       @5
       D=A
       @R13
-      A=A-D // A register points to FRAME-5
+      A=M-D // A register points to FRAME-5
       D=M // D register holds address at FRAME-5
       @R14 // Temporary variable RET
       M=D
 
       // Set address at ARG to value at SP-1
       @SP
-      A=A-1
+      A=M-1 // A register holds address of stack pointer - 1
       D=M // return value is now in D
       @ARG
       A=M
@@ -35,34 +35,43 @@ class FunctionCall
       @SP
       M=D
 
+      // THAT = *(FRAME-1)
       @R13 // FRAME
-      D+M
+      A=M-1
+      D=M
       @THAT
-      M=D-1
+      M=D
 
       @R13 // FRAME
-      D+M
+      D=M
       @2
       D=D-A
+      A=D
+      D=M
       @THIS
       M=D
 
       @R13 // FRAME
-      D+M
+      D=M
       @3
       D=D-A
+      A=D
+      D=M
       @ARG
       M=D
 
       @R13 // FRAME
-      D+M
+      D=M
       @4
       D=D-A
+      A=D
+      D=M
       @LCL
       M=D
 
       // Go to return address
       @R14
+      A=M
       0;JMP
       CODE
     end
